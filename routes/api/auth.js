e153a90a241444db0ccffad5ef62fbc084f21f6d
TetianaPiatykh <tanya.pjatykh@gmail.com>
2023-01-28 16:35:46 +0200

    for checking avatar

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const ctrl = require('../../controllers/auth');
-const { validation, authenticate } = require('../../middlewares');
+const { validation, authenticate, upload } = require('../../middlewares');
 
 const { signupSchema, loginSchema, subscriptionSchema } = require('../../shemas/users');
 
@@ -14,5 +14,7 @@ router.post('/login', validation(loginSchema), ctrl.login);
 router.get('/current', authenticate, ctrl.getCurrent);
 
 router.post('/logout', authenticate, ctrl.logout);
+
+router.patch('/avatars', authenticate, upload.single('avatar'), ctrl.updateAvatar);
 
 module.exports = router;
